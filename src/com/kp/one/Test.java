806ad7f2ae6aed1879668a44b8fda849c178ce56
Krishnaprasad K <krishnaprasad.k1@ge.com>
2017-10-18 15:57:44 +0530

    simplecommit

---
+++ src/com/kp/one/Test.java
@@ -0,0 +1,13 @@
+package com.kp.one;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(":pokdwd");
+		System.out.println(":pokdwd");
+		System.out.println(":pokdwd");
+		System.out.println(":pokdwd");
+
+	}
+
+}
